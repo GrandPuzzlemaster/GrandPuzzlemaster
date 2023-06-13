b4162c9f81e43379452cd73e2e095e15dfa33f59
GrandPuzzlemaster <zirnworks@gmail.com>
2023-06-13 08:56:11 -0700

    First crack at TFIL archive

---
+++ js/portfolio-additions.js
@@ -0,0 +1,144 @@
+
+(function($, window, document, undefined) {
+    'use strict';
+
+
+/* ==============================================
+INSTAGRAM PORTFOLIO SCRIPTS 6 COLUMNS (Modified by DM)
+=============================================== */
+
+// init cubeportfolio
+    $('#instagram.6-columns #instagram-items').cubeportfolio({
+        filters: '#instagram.6-columns #instagram-filters',
+        loadMoreAction: 'click',
+        loadMore: '#loadMore-container',
+        layoutMode: 'grid',
+        defaultFilter: '*',
+        animationType: 'quicksand',
+        gapHorizontal: 0,
+        gapVertical: 0,
+        gridAdjustment: 'responsive',
+        mediaQueries: [{
+            width: 1600,
+            cols: 6
+        },{
+            width: 1100,
+            cols: 6
+        }, {
+            width: 800,
+            cols: 3
+        }, {
+            width: 500,
+            cols: 2
+        }, {
+            width: 320,
+            cols: 1
+        }],
+        caption: 'zoom',
+        displayType: 'lazyLoading',
+        displayTypeSpeed: 80,
+
+        // lightbox
+        lightboxDelegate: '.cbp-lightbox',
+        lightboxGallery: true,
+        lightboxTitleSrc: 'data-title',
+        lightboxCounter: '<div class="cbp-popup-lightbox-counter">{{current}} of {{total}}</div>',
+
+        // singlePage popup
+        singlePageDelegate: '.cbp-singlePage',
+        singlePageDeeplinking: true,
+        singlePageStickyNavigation: true,
+        singlePageCounter: '<div class="cbp-popup-singlePage-counter">{{current}} of {{total}}</div>',
+        singlePageCallback: function(url, element) {
+            // to update singlePage content use the following method: this.updateSinglePage(yourContent)
+            var t = this;
+
+            $.ajax({
+                    url: url,
+                    type: 'GET',
+                    dataType: 'html',
+                    timeout: 5000
+                })
+                .done(function(result) {
+                    t.updateSinglePage(result);
+                })
+                .fail(function() {
+                    t.updateSinglePage("Error! Please refresh the page!");
+                });
+        }
+    });
+
+
+
+/* ==============================================
+TEAMPHOTO PORTFOLIO SCRIPTS 6 COLUMNS  (modified by DM)
+=============================================== */
+
+    // init cubeportfolio
+    $('#teamphoto.6-columns #teamphoto-items').cubeportfolio({
+        filters: '#teamphoto.6-columns #teamphoto-filters',
+        loadMoreAction: 'click',
+        loadMore: '#loadMore-container',
+        layoutMode: 'grid',
+        defaultFilter: '*',
+        animationType: 'quicksand',
+        gapHorizontal: 0,
+        gapVertical: 0,
+        gridAdjustment: 'responsive',
+        mediaQueries: [{
+            width: 1600,
+            cols: 6
+        },{
+            width: 1100,
+            cols: 6
+        }, {
+            width: 800,
+            cols: 3
+        }, {
+            width: 500,
+            cols: 2
+        }, {
+            width: 320,
+            cols: 1
+        }],
+        caption: 'zoom',
+        displayType: 'lazyLoading',
+        displayTypeSpeed: 80,
+
+        // lightbox
+        lightboxDelegate: '.cbp-lightbox',
+        lightboxGallery: true,
+        lightboxTitleSrc: 'data-title',
+        lightboxCounter: '<div class="cbp-popup-lightbox-counter">{{current}} of {{total}}</div>',
+
+        // singlePage popup
+        singlePageDelegate: '.cbp-singlePage',
+        singlePageDeeplinking: true,
+        singlePageStickyNavigation: true,
+        singlePageCounter: '<div class="cbp-popup-singlePage-counter">{{current}} of {{total}}</div>',
+        singlePageCallback: function(url, element) {
+            // to update singlePage content use the following method: this.updateSinglePage(yourContent)
+            var t = this;
+
+            $.ajax({
+                    url: url,
+                    type: 'GET',
+                    dataType: 'html',
+                    timeout: 5000
+                })
+                .done(function(result) {
+                    t.updateSinglePage(result);
+                })
+                .fail(function() {
+                    t.updateSinglePage("Error! Please refresh the page!");
+                });
+        }
+    });
+
+
+//End Function
+})(jQuery, window, document);
+
+
+
+
